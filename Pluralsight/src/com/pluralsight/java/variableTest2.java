3c6054368d2aa86777991e5a90e071f5df361fd2
Astro <45946309+dasmeter@users.noreply.github.com>
2018-12-25 21:22:50 -0700

    updating variable value

--- Pluralsight/src/com/pluralsight/java/variableTest2.java
+++ Pluralsight/src/com/pluralsight/java/variableTest2.java
@@ -3,7 +3,7 @@ package com.pluralsight.java;
 public class variableTest2 {
 
 	public static void main(String[] args) {
-		int valA = 21;
+		int valA = 22;
 		int valB = 6;
 		int valC = 3;
 		int valD = 1;
